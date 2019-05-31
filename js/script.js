8f09f688a638c5fbbb585c3cb7748f89d5305bcd
Sylwia Palczowska <sylwiapalczowska@gmail.com>
2019-05-31 17:00:46 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,81 +1,81 @@
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result")
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("k");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("n");
 
 function getComputerChoice() {
-  const choices = ['kamień', 'papier', 'nożyce'];
-  const randomNumber = Math.floor(Math.random() * 3);
-  return choices [randomNumber];
+    const choices = ['kamień', 'papier', 'nożyce'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
 function convertToWord(letter) {
-  if (letter === "k") return "kamień";
-  if (letter === "p") return "papier";
-  return "nożyce"
+    if (letter === "k") return "kamień";
+    if (letter === "p") return "papier";
+    return "nożyce"
 }
 
 function win(userChoice, computerChoice) {
-  userScore++;
-  userScore_span.innerHTML = userScore;
-  computerScore_span.innerHTML = computerScore;
-  const smallUserWord = "user".fontsize(3).sub();
-  const smallCompWord = "comp".fontsize(3).sub();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} wygrywa z ${convertToWord(computerChoice)}${smallCompWord}. Wygrywasz!`;
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} wygrywa z ${convertToWord(computerChoice)}${smallCompWord}. Wygrywasz!`;
+
 }
 
 function lose(userChoice, computerChoice) {
-  computerScore++;
-  userScore_span.innerHTML = userScore;
-  computerScore_span.innerHTML = computerScore;
-  const smallUserWord = "user".fontsize(3).sub();
-  const smallCompWord = "comp".fontsize(3).sub();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} przegrywa z ${convertToWord(computerChoice)}${smallCompWord}. Przegrywasz!`;
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} przegrywa z ${convertToWord(computerChoice)}${smallCompWord}. Przegrywasz!`;
 }
 
 function draw(userChoice, computerChoice) {
-  userScore_span.innerHTML = userScore;
-  computerScore_span.innerHTML = computerScore;
-  const smallUserWord = "user".fontsize(3).sub();
-  const smallCompWord = "comp".fontsize(3).sub();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} remisuje z ${convertToWord(computerChoice)}${smallCompWord}. Remis!`;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} remisuje z ${convertToWord(computerChoice)}${smallCompWord}. Remis!`;
 }
 
 function game(userChoice) {
-  const computerChoice = getComputerChoice();
-  switch (userChoice + computerChoice) {
-  	case "kn":
-  	case "pk":
-  	case "np":
-  	win(userChoice, computerChoice);
-  	break;
-  	case "kp":
-  	case "pn":
-  	case "nk":
-  	lose(userChoice, computerChoice);
-  	break;
-  	case "kk":
-  	case "pp":
-  	case "nn":
-  	draw(userChoice, computerChoice);
-  }
+    const computerChoice = getComputerChoice();
+
+    switch (userChoice + computerChoice) {
+        case "kn":
+        case "pk":
+        case "np":
+            win(userChoice, computerChoice);
+            break;
+        case "kp":
+        case "pn":
+        case "nk":
+            lose(userChoice, computerChoice);
+            break;
+        case "kk":
+        case "pp":
+        case "nn":
+            draw(userChoice, computerChoice);
+    }
 }
 
-function main() {
-rock_div.addEventListener('click', function(){
-  console.log("kamień!");
+
+rock_div.addEventListener('click', function() {
+    console.log("kliknąłeś na kamień");
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("kliknąłeś na papier");
 });
 
-paper_div.addEventListener('click', function(){
-  console.log("papier!!!");
-});
-
-scissors_div.addEventListener('click', function(){
-  console.log("nożyce!!!!!11");
-});
-}
+scissors_div.addEventListener('click', function() {
+    console.log("kliknąłeś na nożyce");
+})
